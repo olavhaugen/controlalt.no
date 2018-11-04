846294de5189d4aaf6cfd9dbe3df500d747448c2
Olav Haugen <olahau@gmail.com>
2018-11-04 12:02:46 +0100

    Updates

---
+++ precache-manifest.d2e58bfee57463086e5ccbce2057c630.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "134f686ae2935e723071",
+    "url": "/static/js/main.134f686a.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "81e3bfbe1c6e030c4dd299419e0500a8",
+    "url": "/index.html"
+  }
+];
