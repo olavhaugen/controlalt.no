31d6fd49807184a81992e44797f6aef02247b50f
Olav Haugen <olahau@gmail.com>
2018-11-04 12:13:50 +0100

    Bootstrapped create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>CTRL + ALT + INVEST!</h1>
+        <a href="mailto:olav@controlalt.no">olav@controlalt.no</a>
+      </div>
+    );
+  }
+}
+
+export default App;
